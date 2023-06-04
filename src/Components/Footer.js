72ee155e82d8001b34fe4fb2a49fa9d4a21124b2
Marina <marinazizicc@gmail.com>
2023-06-04 23:10:21 +0200

    New components added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,50 @@
 import './css/Footer.css';
+import LogoF from './imgs/logo-footer.png';
+import Instagram from './imgs/instagram.svg';
+import Facebook from './imgs/facebook.svg';
+import YouTube from './imgs/youtube.svg';
 
 function Footer() {
     return (
         <div className="Footer">
+            <div className='motto'>
+                <h2>Ako te netko za zdravlje pita reci samo Cedevita</h2>
+            </div>
+            <hr />
+            <div className='footerGrid'>
+                <div className='socials'>
+                    <img src={LogoF}></img>
+                    <div className='medias'>
+                        <a href="https://instagram.com">
+                            <img src={Instagram}></img>
+                        </a>
+                        <a href="https://facebook.com">
+                            <img src={Facebook}></img>
+                        </a>
+                        <a href="https://youtube.com">
+                            <img src={YouTube}></img>
+                        </a>
+                    </div>
+                    <button>Kontaktiraj nas</button>
+                </div>
+                <div className='about'>
+                    <ul>
+                        <li><a href="#about">Općenito</a></li>
+                        <li><a href="#proizvodi">Proizvodi</a></li>
+                        <li>Novosti</li>
+                        <li>Pravila privatnosti</li>
+                    </ul>
+                    <ul>
+                        <li><a href='#giveaway'>Nagrade</a></li>
+                        <li>Postavke kolačića</li>
+                        <li>Pridruži nam se</li>
+                        <li>Kontakt</li>
+                    </ul>
+                </div>
+                <div className='us'>
+                    <p>Naš cilj je stvoriti proizvode koji nadmašuju očekivanja, inspiriraju ljude i pružaju trenutke užitka u svakoj čaši. Cedevita je više od samo napitka - ona je stil života, izvor energije i pouzdan partner za sve one koji žele osvježiti svoje tijelo i duh.</p>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/Components/Newsletter.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './css/Newsletter.css';
+import NewsletterImg from './imgs/newsletter.jpg';
+
+class Newsletter extends Component {
+    constructor() {
+        super();
+        this.state = {
+            inputValue: '',
+        };
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ inputValue: event.target.value });
+    }
+
+    render() {
+        const { inputValue } = this.state;
+        const image = './imgs/newsletter.jpg';
+
+        return (
+            <div className='Newsletter' id='newsletter'>
+                <div className='textNews'>
+                    <h1>Prijavi se na newsletter</h1>
+                    <p>Naš newsletter vam pruža mogućnost da budete među prvima koji će saznati o najnovijim vijestima, događajima i aktivnostima. Bez obzira o čemu je riječ, uvijek ćete bit obaviješteni na vrijeme.</p>
+                    <form>
+                        <input type="text" value={inputValue} onChange={this.handleInputChange} placeholder='Vaše ime' required/>
+                        <input type='email' placeholder='Vaš e-mail' required></input>
+                        <button>Potvrdi</button>
+                    </form>
+                    <p>Hvala ti {inputValue} na prijavi!</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Newsletter;
