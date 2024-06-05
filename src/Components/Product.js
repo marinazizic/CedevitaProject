99784fd91ad9ad3870943e96931111d24e63e6da
Marina Žižić <117577388+marinazizic@users.noreply.github.com>
2024-06-05 22:06:38 +0000

    Improved responsivness

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,37 +1,39 @@
-import Limun from './Limun.js';
-import Bazga from './Bazga.js';
-import Naranca from './Naranca.js';
-import './css/Products.css';
+import Limun from "./Limun.js";
+import Bazga from "./Bazga.js";
+import Naranca from "./Naranca.js";
+import "./css/Products.css";
 
 function Product() {
-    let NarancaObj = {
-        okus: "Naranča",
-        kalorije: "70g na 100g",
-        rokTrajanja: "2 godine",
-        kolicina: "500g, 1kg, 2kg",
-    };
+  let NarancaObj = {
+    okus: "Naranča",
+    kalorije: "70g na 100g",
+    rokTrajanja: "2 godine",
+    kolicina: "500g, 1kg, 2kg",
+  };
 
-    let LimunObj = {
-        okus: "Limun",
-        kalorije: "58g na 100g",
-        rokTrajanja: "1 godina",
-        kolicina: "500g, 1kg, 2kg",
-    };
+  let LimunObj = {
+    okus: "Limun",
+    kalorije: "58g na 100g",
+    rokTrajanja: "1 godina",
+    kolicina: "500g, 1kg, 2kg",
+  };
 
-    let BazgaObj = {
-        okus: "Bazga",
-        kalorije: "66g na 100g",
-        rokTrajanja: "2 godine",
-        kolicina: "500g, 1kg",
-    };
+  let BazgaObj = {
+    okus: "Bazga",
+    kalorije: "66g na 100g",
+    rokTrajanja: "2 godine",
+    kolicina: "500g, 1kg",
+  };
 
-    return (
-        <div className="Products" id='proizvodi'>
-            <Naranca NarancaProizvod={NarancaObj}></Naranca>
-            <Limun LimunProizvod={LimunObj}></Limun>
-            <Bazga BazgaProizvod={BazgaObj}></Bazga>
-        </div>
-    );
+  return (
+    <div className="Products" id="proizvodi">
+      <div className="CedevitaaProducts">
+        <Naranca NarancaProizvod={NarancaObj}></Naranca>
+        <Limun LimunProizvod={LimunObj}></Limun>
+        <Bazga BazgaProizvod={BazgaObj}></Bazga>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
