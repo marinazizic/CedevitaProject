66796078a78bc61e5569780ff07928d9a22c52ad
Marina Žižić <117577388+marinazizic@users.noreply.github.com>
2024-06-05 21:39:34 +0000

    Changed name to FindMore

---
+++ src/Components/FindMore.js
@@ -0,0 +1,14 @@
+import "./css/FindMore.css";
+import Fico from "./imgs/background.jpg";
+
+function FindMore() {
+  return (
+    <div className="FindMore" id="FindMore">
+      <div className="positioned">
+        <img src={Fico} alt="FindMore"></img>
+      </div>
+    </div>
+  );
+}
+
+export default FindMore;
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,16 +1,16 @@
-import './css/Homepage.css';
-import Cover from './Cover';
-import Aboutus from './Aboutus';
-import Giveaway from './Giveaway';
-import Product from './Product';
-import Newsletter from './Newsletter';
+import "./css/Homepage.css";
+import Cover from "./Cover";
+import Aboutus from "./Aboutus";
+import FindMore from "./FindMore";
+import Product from "./Product";
+import Newsletter from "./Newsletter";
 
 function Homepage() {
   return (
     <div className="Homepage">
       <Cover></Cover>
       <Aboutus></Aboutus>
-      <Giveaway></Giveaway>
+      <FindMore></FindMore>
       <Product></Product>
       <Newsletter></Newsletter>
     </div>
